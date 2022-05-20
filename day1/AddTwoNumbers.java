822ddb05959bd038f609c774e682af52ac8ffab0
Harish <nagaruruharish8499@gmail.com>
2022-05-20 06:53:59 +0000

    first prgam

---
+++ day1/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.demo.add;
+
+public class AddTwoNumbers {
+	public static void main(String arg[]) {
+		int number1=10;
+		int number2=20;
+		int result=number1+number2;
+		System.out.println("sum of two numbers"+ result);
+	}
+
+}
